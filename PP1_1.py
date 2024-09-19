e29ee17c3e887bb53de66288c3648fe552c59f47
PraneethR09 <praneethrameswaran9@gmail.com>
2024-09-19 19:01:32 +0000

    Finished Assignment!

--- PP1_1.py
+++ PP1_1.py
@@ -2,15 +2,21 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-
+  print('Hello World')
 def q2():
   #Write Assignment code here
-  
+  print(1)
+  print(2)
+  print(3)
+  print(4)
+  print(5)
 def q3():
   #Write Assignment code here
-  
+  print(' Help Me!')
 def q4():
+  print('3 x 3 = 9')
   #Write Assignment code here
+
 
 #Do not alter anything after this line
 q1()
